27af6ad575d900d406bbc02975cc6411e275b640
Miguel Montagner <dev@miguelmontagner.com>
2024-10-05 02:10:22 +0100

    added JPA connection

--- src/main/java/com/example/sqldemo/SqldemoApplication.java
+++ src/main/java/com/example/sqldemo/SqldemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.sqldemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SqldemoApplication {
@@ -10,4 +12,10 @@ public class SqldemoApplication {
 		SpringApplication.run(SqldemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
