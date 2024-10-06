92b415b08b3f2dcc10a1b4a6c38f58f2f3a31205
Miguel Montagner <dev@miguelmontagner.com>
2024-10-06 08:50:17 +0100

    Added DAO for student and ran a test for database connection

--- src/main/java/com/example/sqldemo/SqldemoApplication.java
+++ src/main/java/com/example/sqldemo/SqldemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.sqldemo;
 
+import com.example.sqldemo.dao.StudentDAO;
+import com.example.sqldemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,21 @@ public class SqldemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("creating new object student");
+		Student tempStudent = new Student("Paul", "Doe", "this.is@sparta.com");
+		// save the student object
+		System.out.println("Saving student");
+		studentDAO.save(tempStudent);
+		//display id of the saved student
+		System.out.println("saved student. ID is: " + tempStudent.getId());
 	}
 }
